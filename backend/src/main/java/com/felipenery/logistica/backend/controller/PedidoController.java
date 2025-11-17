594ea8cb047a2397142b7b0e5bb9ad554b188af3
Felipe Nery <ffneryy@gmail.com>
2025-11-17 20:23:09 -0300

    Criação Endpoint POST no Controller

---
+++ backend/src/main/java/com/felipenery/logistica/backend/controller/PedidoController.java
@@ -0,0 +1,25 @@
+package com.felipenery.logistica.backend.controller;
+
+import com.felipenery.logistica.backend.domain.entity.Pedido;
+import com.felipenery.logistica.backend.domain.repository.PedidoRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/pedidos")
+
+public class PedidoController {
+
+    private final PedidoRepository pedidoRepository;
+    public PedidoController(PedidoRepository pedidoRepository) {
+        this.pedidoRepository = pedidoRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Pedido> cadastrar(@RequestBody Pedido pedido) {
+        Pedido novoPedido = pedidoRepository.save(pedido);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoPedido);
+    }
+
+}
